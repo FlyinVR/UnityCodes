691b898310ea408d7de0fcc2d82fd4e05bcca22c
wandonye <dongning.wang@gmail.com>
2016-11-16 21:32:03 -0600

    udp client
    not sure if it is working or not

--- Assets/viewpoint.cs
+++ Assets/viewpoint.cs
@@ -1,15 +1,72 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Net;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+public static class GlobalVariables
+{
+    public static float gyroY;
+    public static float gyroR;
+    public static float gyroP;
+
+}
 
 public class viewpoint : MonoBehaviour {
+    Thread receiveThread;
+    UdpClient client;
+    // Use this for initialization
+    void Start () {
+        initUDP();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private void initUDP()
+    {
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        print("Start UDP Client");
+    }
+
+    private void ReceiveData()
+    {
+        int somePort = 6390;
+
+        try
+        {
+
+            client = new UdpClient(somePort);
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+            //client.Client.Blocking = false;
+            while (Thread.CurrentThread.IsAlive)
+            {
+                // do stuff
+                Byte[] receiveBytes = client.Receive(ref RemoteIpEndPoint);
+                string returnData = Encoding.ASCII.GetString(receiveBytes);
+
+                // Uses the IPEndPoint object to determine which of these two hosts responded.
+                Console.WriteLine("This is the message you received " +
+                                             returnData.ToString());
+                Console.WriteLine("This message was sent from " +
+                                            RemoteIpEndPoint.Address.ToString() +
+                                            " on their port number " +
+                                            RemoteIpEndPoint.Port.ToString());
+                Thread.Sleep(10);
+            }
+
+        }
+        catch (Exception e)
+        {
+            print(e);
+        }
+    }
+    // Update is called once per frame
+    void Update () {
         transform.position += transform.forward * Time.deltaTime * 50.0f;
 
         transform.Rotate(-Input.GetAxis("Vertical"), Input.GetAxis("Horizontal"), 0.0f);
@@ -33,4 +90,11 @@ public class viewpoint : MonoBehaviour {
         transform.rotation = rotation;
     }
 
+    public void OnApplicationQuit()
+    {
+        // end of application
+        if (receiveThread != null) receiveThread.Abort();
+        if (client != null) client.Close();
+        print("Stopped");
+    }
 }
