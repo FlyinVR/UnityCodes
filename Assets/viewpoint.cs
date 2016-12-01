cd58950661a93876981e74d985fc2c9571bf1dbc
wandonye <dongning.wang@gmail.com>
2016-12-01 01:41:08 -0600

    two iPhones. shake to reset

--- Assets/viewpoint.cs
+++ Assets/viewpoint.cs
@@ -13,16 +13,24 @@ using System.Text;
 
 public static class GlobalVariables
 {
-	public static double gyroY;
-	public static double gyroR;
-	public static double gyroP;
+	public static double gyroYRight;
+	public static double gyroRRight;
+	public static double gyroPRight;
+
+	public static double gyroYLeft;
+	public static double gyroRLeft;
+	public static double gyroPLeft;
+
+	public static bool shouldReset;
 
 }
 
 public class viewpoint : MonoBehaviour
 {
-	Thread receiveThread;
-	UdpClient client;
+	Thread receiveRightThread;
+	UdpClient clientRight;
+	Thread receiveLeftThread;
+	UdpClient clientLeft;
 
 	private float g = 9.8f;
 	private float density = 1.0f;
@@ -43,34 +51,39 @@ public class viewpoint : MonoBehaviour
 		//up = new Vector3(-1.0f, 10.0f, -1.0f);
 //		tip = Vector3.Cross (up, Vector3.Cross (v, up)).normalized;
 		tip.Normalize();
-		transform.position = new Vector3(813.0f, 500.0f, 874.0f);
+		transform.position = new Vector3(813.0f, 1000.0f, 874.0f);
 		//transform.position = new Vector3(813.0f, 330.0f, 874.0f);
 		Quaternion rotation = Quaternion.LookRotation(v, up);
 		transform.rotation = rotation;
-
-		Input.GetAxis("Vertical");
-		Input.GetAxis("Horizontal");
 
 	}
 
 	// Update is called once per frame
 	void Update()
-	{
+	{	
+		Vector3 axis1 = Vector3.Cross (up, tip);
+		Vector3 axis2 = -tip;
+	
+		tip = Quaternion.AngleAxis(Math.Min(90.0f,Input.GetAxis("Vertical")), axis1)*tip;
+		up = Quaternion.AngleAxis(Input.GetAxis("Horizontal"), axis2)*up;
+
 		float v_up = Vector3.Dot (v, up);
 		Vector3 lift = density * v_up * 0.02f * v_up * up;
 		Vector3 gravity = -g * e_y;
 		Vector3 friction = -fric * Vector3.Dot (v, tip) * tip;
 //		print ("fric:"+friction);
-		print ("lift" + lift);
+//		print ("lift" + lift);
 		v = v + (gravity+lift)*Time.deltaTime*C;
 		transform.position += v * Time.deltaTime;
-		print ("velocity: "+v);
+//		print ("velocity: "+v);
 		Quaternion rotation = Quaternion.LookRotation(v, up);
 		transform.rotation = rotation;
 
 		float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
-		if (terrainHeightWhereWeAre > transform.position.y)
+		if (terrainHeightWhereWeAre > transform.position.y || GlobalVariables.shouldReset)
 		{
+			GlobalVariables.shouldReset = false;
+			print ("should reset");
 			reset();
 		}
 	}
@@ -82,37 +95,91 @@ public class viewpoint : MonoBehaviour
 
 	void reset()
 	{
-		if (receiveThread != null) receiveThread.Abort();
-		if (client != null) client.Close();
+		if (receiveRightThread != null) receiveRightThread.Abort();
+		if (clientRight != null) clientRight.Close();
+		if (receiveLeftThread != null) receiveLeftThread.Abort();
+		if (clientLeft != null) clientLeft.Close();
 		SceneManager.LoadScene(1);
 	}
 
 
 	private void initUDP()
 	{
-		receiveThread = new Thread(
-			new ThreadStart(ReceiveData));
+		receiveRightThread = new Thread(
+			new ThreadStart(ReceiveRightData));
 
-		receiveThread.IsBackground = true;
-		receiveThread.Start();
+		receiveRightThread.IsBackground = true;
+		receiveRightThread.Start();
+
+		receiveLeftThread = new Thread(
+			new ThreadStart(ReceiveLeftData));
+
+		receiveLeftThread.IsBackground = true;
+		receiveLeftThread.Start();
 
 		print("Start UDP Client");
 	}
 
-	private void ReceiveData()
+	private void ReceiveRightData()
 	{
-		int somePort = 6390;
+		int portRight = 6390;
 
 		try
 		{
 
-			client = new UdpClient(somePort);
-			IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+			clientRight = new UdpClient(portRight);
+			IPEndPoint RemoteIpEndPointRight = new IPEndPoint(IPAddress.Any, 0);
 			//client.Client.Blocking = false;
 			while (Thread.CurrentThread.IsAlive)
 			{
 				// do stuff
-				Byte[] receiveBytes = client.Receive(ref RemoteIpEndPoint);
+				Byte[] receiveBytes = clientRight.Receive(ref RemoteIpEndPointRight);
+				if (BitConverter.IsLittleEndian)
+					Array.Reverse(receiveBytes);
+
+				Byte[] slice1 = new List<Byte>(receiveBytes).GetRange(0, 4).ToArray();
+				UInt32 p = BitConverter.ToUInt32(slice1, 0);
+				//				print(p);
+				Byte[] slice2 = new List<Byte>(receiveBytes).GetRange(4, 4).ToArray();
+				UInt32 y = BitConverter.ToUInt32(slice2, 0);
+//								print(y);
+				Byte[] slice3 = new List<Byte>(receiveBytes).GetRange(8, 4).ToArray();
+				UInt32 r = BitConverter.ToUInt32(slice3, 0);
+//								print(r);
+
+				if (r<UInt32.MaxValue && y<UInt32.MaxValue && p<UInt32.MaxValue){
+					GlobalVariables.gyroRRight = (r - 180000.0) / 1000.0;
+					GlobalVariables.gyroYRight = (y - 180000.0) / 1000.0;
+					GlobalVariables.gyroPRight = (p - 180000.0) / 1000.0;
+				}else{
+					print("reset");
+					GlobalVariables.shouldReset = true;
+				}
+
+
+				Thread.Sleep(10);
+			}
+
+		}
+		catch (Exception e)
+		{
+			print(e);
+		}
+	}
+	private void ReceiveLeftData()
+	{
+		int portLeft = 6391;
+
+		try
+		{
+
+			clientLeft = new UdpClient(portLeft);
+			IPEndPoint RemoteIpEndPointLeft = new IPEndPoint(IPAddress.Any, 0);
+			//client.Client.Blocking = false;
+			while (Thread.CurrentThread.IsAlive)
+			{
+				// do stuff
+				Byte[] receiveBytes = clientLeft.Receive(ref RemoteIpEndPointLeft);
 				if (BitConverter.IsLittleEndian)
 					Array.Reverse(receiveBytes);
 
@@ -126,22 +193,10 @@ public class viewpoint : MonoBehaviour
 				Int32 r = BitConverter.ToInt32(slice3, 0);
 				//				print(r);
 
-				GlobalVariables.gyroR = (r - 180000.0) / 1000.0;
-				GlobalVariables.gyroY = (y - 180000.0) / 1000.0;
-				GlobalVariables.gyroP = (p - 180000.0) / 1000.0;
+				GlobalVariables.gyroRLeft = (r - 180000.0) / 1000.0;
+				GlobalVariables.gyroYLeft = (y - 180000.0) / 1000.0;
+				GlobalVariables.gyroPLeft = (p - 180000.0) / 1000.0;
 
-				//				string returnData = Encoding.UTF8.GetString(receiveBytes);
-				//				print(returnData);
-				//				var base64EncodedBytes = System.Convert.FromBase64String(returnData);
-				//				print(System.Text.Encoding.UTF8.GetString(base64EncodedBytes));
-				//
-				//                // Uses the IPEndPoint object to determine which of these two hosts responded.
-				//				print("This is the message you received " +
-				//                                             returnData.ToString());
-				//				print("This message was sent from " +
-				//                                            RemoteIpEndPoint.Address.ToString() +
-				//                                            " on their port number " +
-				//                                            RemoteIpEndPoint.Port.ToString());
 				Thread.Sleep(10);
 			}
 
@@ -151,12 +206,13 @@ public class viewpoint : MonoBehaviour
 			print(e);
 		}
 	}
-
 	public void OnApplicationQuit()
 	{
 		// end of application
-		if (receiveThread != null) receiveThread.Abort();
-		if (client != null) client.Close();
+		if (receiveRightThread != null) receiveRightThread.Abort();
+		if (clientRight != null) clientRight.Close();
+		if (receiveLeftThread != null) receiveLeftThread.Abort();
+		if (clientLeft != null) clientLeft.Close();
 		// print("Stopped");
 	}
 }
